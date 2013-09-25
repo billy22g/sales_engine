4bf215418aea508f09b983bf3e461c83c9d2e8ba
Billy <billygriffin@Laptop.local>
2013-09-25 15:48:09 -0600

    fixed merchant repo test and merchant repo code

--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -21,17 +21,17 @@ class MerchantRepoTest < Minitest::Test
 
   def test_that_merchant_objects_method_actually_creates_new_merchant_objects
     merchant = repo.merchant_objects
-    assert_equal "5", merchant.last.merchant_id
-    assert_equal "Williamson Group", merchant.last.merchant_name
+    assert_equal "5", merchant.last.id
+    assert_equal "Williamson Group", merchant.last.name
   end
 
   def test_that_it_finds_one_merchant_by_attribute
-    merchant = repo.find_by(:merchant_name, "Williamson Group")
-    assert_equal "5", merchant.merchant_id
+    merchant = repo.find_by_name("Williamson Group")
+    assert_equal "5", merchant.id
   end
 
   def test_that_it_finds_all_merchants_by_attribute
-    merchant = repo.find_all_by(:merchant_created_at, "2012-03-27 14:53:59 UTC")
+    merchant = repo.find_all_by_created_at("2012-03-27 14:53:59 UTC")
     assert_equal 5, merchant.length
   end
 
