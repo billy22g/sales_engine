707d9861a0f891661b080acd37ce2082b4087c72
Billy <navyosu@gmail.com>
2013-09-27 18:27:24 -0600

    Added Transactions Repo and Test Files

---
+++ lib/transaction_repo.rb
@@ -0,0 +1,110 @@
+require "csv"
+require "./lib/transaction"
+require "pry"
+
+class TransactionRepo
+  
+  def initialize(filename = "./data/transactions.csv")
+    @transaction_list = []
+    @filename = filename
+    transaction_objects
+  end
+
+  def read_file
+    @transaction_data = CSV.read "./data/transactions.csv", headers: true, header_converters: :symbol
+  end
+
+  def transaction_objects
+    if @transaction_list.empty?
+      read_file.each do |row|
+        @transaction_list << Transaction.new(row)
+      end
+    end
+    return @transaction_list
+  end
+
+  def all
+    transaction_objects
+  end
+
+  def filename
+    @filename
+  end
+
+  def transaction_list
+    @transaction_list
+  end
+
+  def random
+    @transaction_list.sample
+  end
+
+  def find_by(attribute, input)
+    transaction_objects.find do |m|
+      m.send(attribute).downcase == input.downcase
+    end
+  end
+
+  def find_by_id(input)
+    find_by(:id, input)
+  end
+
+  def find_by_invoice_id(input)
+    find_by(:invoice_id, input)
+  end
+
+  def find_by_credit_card_number(input)
+    find_by(:credit_card_number, input)
+  end
+
+  def find_by_credit_card_expiration_date(input)
+    find_by(:credit_card_expiration_date, input)
+  end
+
+  def find_by_result(input)
+    find_by(:result, input)
+  end
+
+  def find_by_created_at(input)
+    find_by(:created_at, input)
+  end
+
+  def find_by_updated_at(input)
+    find_by(:updated_at, input)
+  end
+
+  def find_all_by(attribute, input)
+    transaction_objects.select do |transaction|
+      transaction.send(attribute).downcase == input.downcase
+    end
+  end
+
+   def find_all_by_id(input)
+    find_all_by(:id, input)
+  end
+
+  def find_all_by_invoice_id(input)
+    find_all_by(:invoice_id, input)
+  end
+
+  def find_all_by_credit_card_number(input)
+    find_all_by(:credit_card_number, input)
+  end
+
+  def find_all_by_credit_card_expiration_date(input)
+    find_all_by(:credit_card_expiration_date, input)
+  end
+
+  def find_all_by_result(input)
+    find_all_by(:result, input)
+  end
+
+  def find_all_by_created_at(input)
+    find_all_by(:created_at, input)
+  end
+
+  def find_all_by_updated_at(input)
+    find_all_by(:updated_at, input)
+  end
+
+end
---
+++ test/transaction_repo_test.rb
@@ -0,0 +1,44 @@
+gem "minitest"
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/transaction_repo"
+require "csv"
+
+class TransactionRepoTest < Minitest::Test 
+
+  def repo
+    @mr ||= TransactionRepo.new("./data/transactions.csv")
+  end
+
+  def test_it_receives_filename_that_will_be_loaded_by_default
+    assert_equal "./data/transactions.csv", TransactionRepo.new.filename
+  end
+
+  def test_it_reads_file
+    loaded_data = CSV.read "./data/transactions.csv", headers: true, header_converters: :symbol
+    assert_equal loaded_data, repo.read_file
+  end
+
+  def test_that_transaction_objects_method_actually_creates_new_transaction_objects
+    transaction = repo.transaction_objects
+    assert_equal "5", transaction.last.id
+    assert_equal "2012-03-28 14:54:10 UTC", transaction.last.created_at
+  end
+
+  def test_that_it_finds_one_transaction_by_attribute
+    transaction = repo.find_by_updated_at("2012-03-30 14:54:10 UTC")
+    assert_equal "5", transaction.id
+  end
+
+  def test_that_it_finds_all_transactions_by_attribute
+    transaction = repo.find_all_by_created_at("2012-03-27 14:54:10 UTC")
+    assert_equal 2, transaction.length
+  end
+
+  def test_that_random_method_returns_a_random_transaction
+    results = []
+    10.times{ results << repo.random }
+    assert results.uniq.count > 1
+  end
+
+end
